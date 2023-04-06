202644ecb2a5be113bdf1aa5d60f899adaef0ee6
Strunin Andrey <strunin10@rambler.ru>
2023-04-06 19:55:00 +0300

    version 0.3

--- src/components/Navmenu.jsx
+++ src/components/Navmenu.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import logo from "./img/logo.svg"
+
 function Navmenu(){
     return(
         <div class="nav-menu">
-        <a href="#main-section" class = "logo-link"><image src="img/logo.svg" alt="logo" class="nav-logo"/></a>
+        <a href="#main-section" class = "logo-link"><img src={logo} alt="logo" class="nav-logo"/></a>
             <div class="nav-container">
                 <a href="#about" class="nav-item">О нас</a>
                 <a href="#doing" class="nav-item">Что мы делаем</a>
